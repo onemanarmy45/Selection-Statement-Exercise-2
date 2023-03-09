4e3c9d694f46a0eb04dad6abc5efda9643aa23cd
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-03-09 13:40:39 -0500

    selection

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementsExercise2
+﻿using Internal;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine($"Math is marvelous!");
+                    break;
+                case "Science":
+                    Console.WriteLine($"Science is superb!");
+                    break;
+                case "Spanish":
+                    Console.WriteLine($"Spanish is spectacular!");
+                    break;
+                case "Culinary":
+                    Console.WriteLine($"Culinary is cowabunga!");
+                    break;
+                default:
+                    Console.WriteLine($"So cool! I've never taken {subject} before!");
+                    break;
+            }
         }
     }
 }
